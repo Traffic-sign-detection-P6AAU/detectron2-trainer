795f97118adf8b16691fba8b0d03fc063bda9f84
knielsen <74406677+k-nielsen@users.noreply.github.com>
2023-05-02 10:13:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from trainer.testing import prepare_dataset, train, evaluate
+from detectron2.config import get_cfg
+
+CATEGORIES_PATH = 'data_handler/categories.json'
+
+def main():
+    cfg = get_cfg()
+    print('---Menu list---')
+    print('Type: 1 to prepare dataset and train')
+    print('Type: 2 to evaluate the model')
+    choice = input()
+    if choice == '1':
+        prepare_dataset()
+        train(cfg)
+        evaluate(cfg)
+    elif choice == '2':
+        evaluate(cfg)
+    # elif choice == '3':
+    #     split_dataset(CATEGORIES_PATH)
+    # elif choice == '4':
+    #     extend_annotations()
+    else:
+        print('Input was not 1, 2, 3 or 4.')
+
+if __name__ == '__main__':
+    main()
